d7c48e6dce06b576a026007fbe88673a64d1166d
Arunprasad <sundharama19@students.ecu.edu>
2021-04-06 01:53:00 -0400

    no message

--- HomeWork5-SurveyData/main.py
+++ HomeWork5-SurveyData/main.py
@@ -19,12 +19,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] =\
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG'] = True
 
-
-
-
-
-# The db object instantiated from the class SQLAlchemy represents the database and
-# provides access to all the functionality of Flask-SQLAlchemy.
 db = SQLAlchemy(app)
 
 
@@ -77,7 +71,7 @@ def home():
     group1_clusters=[]
     group1recordsByCountry=getGroup1RecordsCountbyCountry()
     for totalrecords in group1recordsByCountry:
-        if totalrecords[1]>=10:
+        if totalrecords[1]>10:
             kmeans_calc_group=[]
             for item in group1_list:
                 if item[1] == totalrecords[0]:                    
@@ -93,7 +87,7 @@ def home():
     group2_clusters=[]
     group2recordsByCountry=getGroup2RecordsCountbyCountry()
     for totalrecords in group2recordsByCountry:
-        if totalrecords[1]>=10:
+        if totalrecords[1]>10:
             kmeans_calc_group=[]
             for item in group2_list:
                 if item[1] == totalrecords[0]:                    
@@ -110,7 +104,7 @@ def home():
     group3_clusters=[]
     group3recordsByCountry=getGroup3RecordsCountbyCountry()
     for totalrecords in group3recordsByCountry:
-        if totalrecords[1]>=10:
+        if totalrecords[1]>10:
             kmeans_calc_group=[]
             for item in group3_list:
                 if item[1] == totalrecords[0]:                    
@@ -128,7 +122,7 @@ def home():
     group4_clusters=[]
     group4recordsByCountry=getGroup4RecordsCountbyCountry()
     for totalrecords in group4recordsByCountry:
-        if totalrecords[1]>=10:
+        if totalrecords[1]>10:
             kmeans_calc_group=[]
             for item in group4_list:
                 if item[1] == totalrecords[0]:                    
@@ -138,45 +132,20 @@ def home():
             group_clusters = util.split_user_data(kmeans_calc_group,group_labels)
             group4_clusters.append(group_clusters) 
         
-    #records=User.query.all()
-        
     
-   
-    #return render_template('index.html',group1=group1_list,group2=group2_list,group3=group3_list,group4=group4_list,countries=country_list) 
-    #return render_template('index.html',group1=group1_list,country_list1=group1recordsByCountry,group1Kmeans=group1_clusters,group2=group2_list,country_list2=country_list2,group3=group3_list,country_list3=country_list3,group4=group4_list,country_list4=country_list4) 
     return render_template('index.html',group1=group1_list,country_list1=group1recordsByCountry,group1Kmeans=group1_clusters,group2=group2_list,country_list2=group2recordsByCountry,group2Kmeans=group2_clusters,group3=group3_list,country_list3=group3recordsByCountry,group3Kmeans=group3_clusters,group4=group4_list,country_list4=group4recordsByCountry,group4Kmeans=group4_clusters) 
-    #return render_template('index.html',group1=group1_list,country_list1=country_list1,group2=group2_list,country_list2=country_list2) 
-           
-@app.route('/upload', methods=['POST'])
-def upload_csv():
     
-    """ with open('survey.csv', 'r') as file:
-        csv_reader = csv.reader(file, delimiter=',')
-        for row in csv_reader:
-            user = User(index=row[0], country=row[1],age = row[2], fear = row[3], anxious = row[4], anger = row[5], happiness = row[6], sadness = row[7], emotionalBigImpact = row[8], reason = row[9], mostMeaning = row[10], 
-            occupation = row[11], addField1 = row[12], addField2 = row[13], addField3 = row[14])
-            db.session.add(user)
-            db.session.commit() """
-    #csv_file = request.files['survey.csv']
-    #csv_file = TextIOWrapper(csv_file, encoding='utf-8')
-    #app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
-    #csv_reader = csv.reader('survey.csv', delimiter=',')
-    #for row in csv_reader:
-        #user = User(username=row[0], email=row[1])
-        #db.session.add(user)
-        #db.session.commit()
-    #return redirect(url_for('upload_csv'))
-    return render_template('index.html', log_html=User.query.all())
+           
+
     
 
 def getGroup1RecordsCountbyCountry():
    
     records=User.query.with_entities(User.country, db.func.count(User.country).label('total')).filter(User.age<=35).filter(User.gender=='Male').group_by(User.country).all();
-    #records=surveyUser.query.filter(temp1).filter(temp2).all()
+    
     list1=[]
     
-    #group3_records=User.query.filter(User.age<=35).filter(User.gender=='Female').group_by(User.country).all()
-    #group4_records=User.query.filter(User.age>=36).filter(User.gender=='Female').group_by(User.country).all()
+    
     for record in records:
         list2=[]        
         list2.append(record.country)
@@ -189,11 +158,10 @@ def getGroup1RecordsCountbyCountry():
 def getGroup2RecordsCountbyCountry():
    
     records=User.query.with_entities(User.country, db.func.count(User.country).label('total')).filter(User.age>=36).filter(User.gender=='Male').group_by(User.country).all();
-    #records=surveyUser.query.filter(temp1).filter(temp2).all()
+    
     list1=[]
     
-    #group3_records=User.query.filter(User.age<=35).filter(User.gender=='Female').group_by(User.country).all()
-    #group4_records=User.query.filter(User.age>=36).filter(User.gender=='Female').group_by(User.country).all()
+    
     for record in records:
         list2=[]        
         list2.append(record.country)
@@ -207,11 +175,10 @@ def getGroup2RecordsCountbyCountry():
 def getGroup3RecordsCountbyCountry():
    
     records=User.query.with_entities(User.country, db.func.count(User.country).label('total')).filter(User.age<=35).filter(User.gender=='Female').group_by(User.country).all();
-    #records=surveyUser.query.filter(temp1).filter(temp2).all()
+    
     list1=[]
     
-    #group3_records=User.query.filter(User.age<=35).filter(User.gender=='Female').group_by(User.country).all()
-    #group4_records=User.query.filter(User.age>=36).filter(User.gender=='Female').group_by(User.country).all()
+    
     for record in records:
         list2=[]        
         list2.append(record.country)
@@ -225,11 +192,10 @@ def getGroup3RecordsCountbyCountry():
 def getGroup4RecordsCountbyCountry():
    
     records=User.query.with_entities(User.country, db.func.count(User.country).label('total')).filter(User.age>=36).filter(User.gender=='Female').group_by(User.country).all();
-    #records=surveyUser.query.filter(temp1).filter(temp2).all()
+    
     list1=[]
     
-    #group3_records=User.query.filter(User.age<=35).filter(User.gender=='Female').group_by(User.country).all()
-    #group4_records=User.query.filter(User.age>=36).filter(User.gender=='Female').group_by(User.country).all()
+    
     for record in records:
         list2=[]        
         list2.append(record.country)
@@ -243,11 +209,10 @@ def getGroup4RecordsCountbyCountry():
 def getGroup1Records():
    
     records=User.query.filter(User.age<=35).filter(User.gender=='Male').all()
-    #records=surveyUser.query.filter(temp1).filter(temp2).all()
+    
     list1=[]
     country_list=set()
-    #group3_records=User.query.filter(User.age<=35).filter(User.gender=='Female').group_by(User.country).all()
-    #group4_records=User.query.filter(User.age>=36).filter(User.gender=='Female').group_by(User.country).all()
+    
     for record in records:
         list2=[]        
         list2.append(record.index)
@@ -275,11 +240,10 @@ def getGroup1Records():
 def getGroup2Records():
    
     records=User.query.filter(User.age>=36).filter(User.gender=='Male').all()
-    #records=surveyUser.query.filter(temp1).filter(temp2).all()
+    
     list1=[]
     country_list=set()
-    #group3_records=User.query.filter(User.age<=35).filter(User.gender=='Female').group_by(User.country).all()
-    #group4_records=User.query.filter(User.age>=36).filter(User.gender=='Female').group_by(User.country).all()
+    
     for record in records:
         list2=[]        
         list2.append(record.index)
@@ -308,11 +272,10 @@ def getGroup2Records():
 def getGroup3Records():
    
     records=User.query.filter(User.age<=35).filter(User.gender=='Female').all()
-    #records=surveyUser.query.filter(temp1).filter(temp2).all()
+    
     list1=[]
     country_list=set()
-    #group3_records=User.query.filter(User.age<=35).filter(User.gender=='Female').group_by(User.country).all()
-    #group4_records=User.query.filter(User.age>=36).filter(User.gender=='Female').group_by(User.country).all()
+    
     for record in records:
         list2=[]        
         list2.append(record.index)
@@ -340,11 +303,10 @@ def getGroup3Records():
 def getGroup4Records():
    
     records=User.query.filter(User.age>=36).filter(User.gender=='Female').all()
-    #records=surveyUser.query.filter(temp1).filter(temp2).all()
+    
     list1=[]
     country_list=set()
-    #group3_records=User.query.filter(User.age<=35).filter(User.gender=='Female').group_by(User.country).all()
-    #group4_records=User.query.filter(User.age>=36).filter(User.gender=='Female').group_by(User.country).all()
+    
     for record in records:
         list2=[]        
         list2.append(record.index)
@@ -370,19 +332,9 @@ def getGroup4Records():
 
 
 
-""" def getCountries:
-    country_records=User.query.filter(User.age<=35).filter(User.country).distinct()
 
-    list1=[]
-   
 
-    for country_record in country_records:             
-        list1.append(country_record.country)
-
-    return list1 """
-
-if __name__ == '__main__':
-    # get current app directory
+if __name__ == '__main__':    
     
     if app.debug:
         app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
