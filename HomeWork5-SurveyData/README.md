e1e2a384ba30bf63de248c7fd75cdf9a3af9b06b
Ryan McLean <mcleanry16@students.ecu.edu>
2021-04-10 15:21:38 -0400

    updated the readme

--- HomeWork5-SurveyData/README.md
+++ HomeWork5-SurveyData/README.md
@@ -4,6 +4,15 @@ Example 39: This project shows how to integrate a database in your web app.
 ### Local Test Setup
 There is no setup required for the database if you have the requirements installed then, the python file will create the database locally.
 
+The required libraries are:
+```
+Flask==1.0.2
+Jinja2==2.10
+flask-sqlalchemy
+sklearn
+```
+They are in requirements.txt which will help with easy of install.
+
 First, we need to install a Python 3 virtual environment with:
 ```
 sudo apt update
@@ -29,4 +38,27 @@ Because we are now inside a virtual environment. We do not need sudo.
 Then you can start the server with:
 ```
 python3 main.py
+```
+
+The results are split up into 4 tables:
+
+Table 1: Males with Age less than or equal to 35
+```
+Grouped By Country and Clustered using Kmeans for groups with more than 10 records.
+First 15 records are displayed here. Please click on the pagination to access next set of records in each table
+```
+Table 2: Males with Age Greater than or equal to 36
+```
+Grouped By Country and Clustered using Kmeans for groups with more than 10 records.
+First 15 records are displayed here. Please click on the pagination to access next set of records in each table
+```
+Table 3: Females with Age less than or equal to 35
+```
+Grouped By Country and Clustered using Kmeans for groups with more than 10 records.
+First 15 records are displayed here. Please click on the pagination to access next set of records in each table
+```
+Table 4: Females with Age greater than or equal to 36
+```
+Grouped By Country and Clustered using Kmeans for groups with more than 10 records.
+First 15 records are displayed here. Please click on the pagination to access next set of records in each table
 ```
--- HomeWork5-SurveyData/main.py
+++ HomeWork5-SurveyData/main.py
@@ -31,11 +31,11 @@ db = SQLAlchemy(app)
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)    
-    index = db.Column(db.String(80))    
-    country = db.Column(db.String(120))  
+    id = db.Column(db.Integer, primary_key=True)
+    index = db.Column(db.String(80))
+    country = db.Column(db.String(120))
     age = db.Column(db.Integer)
-    gender = db.Column(db.String(120)) 
+    gender = db.Column(db.String(120))
     fear = db.Column(db.Integer)
     anxious = db.Column(db.Integer)
     anger = db.Column(db.Integer)
@@ -48,110 +48,110 @@ class User(db.Model):
     addField1 = db.Column(db.String(500))
     addField2 = db.Column(db.String(500))
     addField3 = db.Column(db.String(500))
-    
+
 
 db.create_all()
 
 app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
-with open('survey.csv', 'r') as file:
+with open('survey.csv', 'rb') as file:
     csv_reader = csv.reader(file, delimiter=',')
     for row in csv_reader:
         user = User(index=row[0], country=row[1],age = row[2],gender=row[3], fear = row[4], anxious = row[6], anger = row[6], happiness = row[7], sadness = row[8], emotionalBigImpact = row[9], reason = row[10], mostMeaning = row[11], occupation = row[12], addField1 = row[13], addField2 = row[14], addField3 = row[15])
         db.session.add(user)
-        db.session.commit()    
-    
-    
+        db.session.commit()
+
+
     """ def __repr__(self):
-        
+
         return "[{},{}]".format(self.index,self.country) """
 
 
 @app.route('/', methods=['GET'])
 def home():
-    
+
 
     group1_list,country_list1= getGroup1Records()
-    
+
     group1_clusters=[]
     group1recordsByCountry=getGroup1RecordsCountbyCountry()
     for totalrecords in group1recordsByCountry:
         if totalrecords[1]>10:
             kmeans_calc_group=[]
             for item in group1_list:
-                if item[1] == totalrecords[0]:                    
+                if item[1] == totalrecords[0]:
                     kmeans_calc_group.append(item)
 
             group_labels = util.cluster_user_data(kmeans_calc_group)
             group_clusters = util.split_user_data(kmeans_calc_group,group_labels)
             group1_clusters.append(group_clusters)
 
-    
+
     group2_list,country_list2= getGroup2Records()
-    
+
     group2_clusters=[]
     group2recordsByCountry=getGroup2RecordsCountbyCountry()
     for totalrecords in group2recordsByCountry:
         if totalrecords[1]>10:
             kmeans_calc_group=[]
             for item in group2_list:
-                if item[1] == totalrecords[0]:                    
+                if item[1] == totalrecords[0]:
                     kmeans_calc_group.append(item)
 
             group_labels = util.cluster_user_data(kmeans_calc_group)
             group_clusters = util.split_user_data(kmeans_calc_group,group_labels)
-            group2_clusters.append(group_clusters)     
+            group2_clusters.append(group_clusters)
 
-    
+
 
     group3_list,country_list3= getGroup3Records()
-    
+
     group3_clusters=[]
     group3recordsByCountry=getGroup3RecordsCountbyCountry()
     for totalrecords in group3recordsByCountry:
         if totalrecords[1]>10:
             kmeans_calc_group=[]
             for item in group3_list:
-                if item[1] == totalrecords[0]:                    
+                if item[1] == totalrecords[0]:
                     kmeans_calc_group.append(item)
 
             group_labels = util.cluster_user_data(kmeans_calc_group)
             group_clusters = util.split_user_data(kmeans_calc_group,group_labels)
-            group3_clusters.append(group_clusters) 
+            group3_clusters.append(group_clusters)
 
-    
+
 
 
     group4_list,country_list4= getGroup4Records()
-    
+
     group4_clusters=[]
     group4recordsByCountry=getGroup4RecordsCountbyCountry()
     for totalrecords in group4recordsByCountry:
         if totalrecords[1]>10:
             kmeans_calc_group=[]
             for item in group4_list:
-                if item[1] == totalrecords[0]:                    
+                if item[1] == totalrecords[0]:
                     kmeans_calc_group.append(item)
 
             group_labels = util.cluster_user_data(kmeans_calc_group)
             group_clusters = util.split_user_data(kmeans_calc_group,group_labels)
-            group4_clusters.append(group_clusters) 
-        
-    
-    return render_template('index.html',group1=group1_list,country_list1=group1recordsByCountry,group1Kmeans=group1_clusters,group2=group2_list,country_list2=group2recordsByCountry,group2Kmeans=group2_clusters,group3=group3_list,country_list3=group3recordsByCountry,group3Kmeans=group3_clusters,group4=group4_list,country_list4=group4recordsByCountry,group4Kmeans=group4_clusters) 
-    
-           
+            group4_clusters.append(group_clusters)
 
-    
+
+    return render_template('index.html',group1=group1_list,country_list1=group1recordsByCountry,group1Kmeans=group1_clusters,group2=group2_list,country_list2=group2recordsByCountry,group2Kmeans=group2_clusters,group3=group3_list,country_list3=group3recordsByCountry,group3Kmeans=group3_clusters,group4=group4_list,country_list4=group4recordsByCountry,group4Kmeans=group4_clusters)
+
+
+
+
 
 def getGroup1RecordsCountbyCountry():
-   
+
     records=User.query.with_entities(User.country, db.func.count(User.country).label('total')).filter(User.age<=35).filter(User.gender=='Male').group_by(User.country).all();
-    
+
     list1=[]
-    
-    
+
+
     for record in records:
-        list2=[]        
+        list2=[]
         list2.append(record.country)
         list2.append(record.total)
         list1.append(list2)
@@ -160,14 +160,14 @@ def getGroup1RecordsCountbyCountry():
 
 
 def getGroup2RecordsCountbyCountry():
-   
+
     records=User.query.with_entities(User.country, db.func.count(User.country).label('total')).filter(User.age>=36).filter(User.gender=='Male').group_by(User.country).all();
-    
+
     list1=[]
-    
-    
+
+
     for record in records:
-        list2=[]        
+        list2=[]
         list2.append(record.country)
         list2.append(record.total)
         list1.append(list2)
@@ -177,14 +177,14 @@ def getGroup2RecordsCountbyCountry():
 
 
 def getGroup3RecordsCountbyCountry():
-   
+
     records=User.query.with_entities(User.country, db.func.count(User.country).label('total')).filter(User.age<=35).filter(User.gender=='Female').group_by(User.country).all();
-    
+
     list1=[]
-    
-    
+
+
     for record in records:
-        list2=[]        
+        list2=[]
         list2.append(record.country)
         list2.append(record.total)
         list1.append(list2)
@@ -194,14 +194,14 @@ def getGroup3RecordsCountbyCountry():
 
 
 def getGroup4RecordsCountbyCountry():
-   
+
     records=User.query.with_entities(User.country, db.func.count(User.country).label('total')).filter(User.age>=36).filter(User.gender=='Female').group_by(User.country).all();
-    
+
     list1=[]
-    
-    
+
+
     for record in records:
-        list2=[]        
+        list2=[]
         list2.append(record.country)
         list2.append(record.total)
         list1.append(list2)
@@ -211,14 +211,14 @@ def getGroup4RecordsCountbyCountry():
 
 
 def getGroup1Records():
-   
+
     records=User.query.filter(User.age<=35).filter(User.gender=='Male').all()
-    
+
     list1=[]
     country_list=set()
-    
+
     for record in records:
-        list2=[]        
+        list2=[]
         list2.append(record.index)
         list2.append(record.country)
         country_list.add(record.country)
@@ -227,29 +227,29 @@ def getGroup1Records():
         list2.append(record.fear)
         list2.append(record.anxious)
         list2.append(record.anger)
-        list2.append(record.happiness)	
+        list2.append(record.happiness)
         list2.append(record.sadness)
         list2.append(record.emotionalBigImpact)
         list2.append(record.reason)
         list2.append(record.mostMeaning)
         list2.append(record.occupation)
         list2.append(record.addField1)
-        list2.append(record.addField2)					
-        list2.append(record.addField3)	
+        list2.append(record.addField2)
+        list2.append(record.addField3)
         list1.append(list2)
 
     return list1,country_list
 
 
 def getGroup2Records():
-   
+
     records=User.query.filter(User.age>=36).filter(User.gender=='Male').all()
-    
+
     list1=[]
     country_list=set()
-    
+
     for record in records:
-        list2=[]        
+        list2=[]
         list2.append(record.index)
         list2.append(record.country)
         country_list.add(record.country)
@@ -258,15 +258,15 @@ def getGroup2Records():
         list2.append(record.fear)
         list2.append(record.anxious)
         list2.append(record.anger)
-        list2.append(record.happiness)	
+        list2.append(record.happiness)
         list2.append(record.sadness)
         list2.append(record.emotionalBigImpact)
         list2.append(record.reason)
         list2.append(record.mostMeaning)
         list2.append(record.occupation)
         list2.append(record.addField1)
-        list2.append(record.addField2)					
-        list2.append(record.addField3)	
+        list2.append(record.addField2)
+        list2.append(record.addField3)
         list1.append(list2)
 
     return list1,country_list
@@ -274,14 +274,14 @@ def getGroup2Records():
 
 
 def getGroup3Records():
-   
+
     records=User.query.filter(User.age<=35).filter(User.gender=='Female').all()
-    
+
     list1=[]
     country_list=set()
-    
+
     for record in records:
-        list2=[]        
+        list2=[]
         list2.append(record.index)
         list2.append(record.country)
         country_list.add(record.country)
@@ -290,29 +290,29 @@ def getGroup3Records():
         list2.append(record.fear)
         list2.append(record.anxious)
         list2.append(record.anger)
-        list2.append(record.happiness)	
+        list2.append(record.happiness)
         list2.append(record.sadness)
         list2.append(record.emotionalBigImpact)
         list2.append(record.reason)
         list2.append(record.mostMeaning)
         list2.append(record.occupation)
         list2.append(record.addField1)
-        list2.append(record.addField2)					
-        list2.append(record.addField3)	
+        list2.append(record.addField2)
+        list2.append(record.addField3)
         list1.append(list2)
 
-    return list1,country_list  
+    return list1,country_list
 
 
 def getGroup4Records():
-   
+
     records=User.query.filter(User.age>=36).filter(User.gender=='Female').all()
-    
+
     list1=[]
     country_list=set()
-    
+
     for record in records:
-        list2=[]        
+        list2=[]
         list2.append(record.index)
         list2.append(record.country)
         country_list.add(record.country)
@@ -321,15 +321,15 @@ def getGroup4Records():
         list2.append(record.fear)
         list2.append(record.anxious)
         list2.append(record.anger)
-        list2.append(record.happiness)	
+        list2.append(record.happiness)
         list2.append(record.sadness)
         list2.append(record.emotionalBigImpact)
         list2.append(record.reason)
         list2.append(record.mostMeaning)
         list2.append(record.occupation)
         list2.append(record.addField1)
-        list2.append(record.addField2)					
-        list2.append(record.addField3)	
+        list2.append(record.addField2)
+        list2.append(record.addField3)
         list1.append(list2)
 
     return list1,country_list
@@ -338,8 +338,8 @@ def getGroup4Records():
 
 
 
-if __name__ == '__main__':    
-    
+if __name__ == '__main__':
+
     if app.debug:
         app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
     app.run(debug = True, port=5287)
